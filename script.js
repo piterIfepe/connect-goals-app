cb4fc3f02c450563b9eb35dbc4e5feda3182301a
piterIfepe <piterifepe@gmail.com>
2025-12-02 18:07:40 +0000

    Adiciona funcionalidade do botão

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 window.onload = function() {
     alert("Bem-vindo à Connect Goals App MVP!");
 };
+
+// Alerta quando o botão for clicado
+document.getElementById("btnMensagem").onclick = function() {
+    alert("Você clicou no botão! 🎯");
+};
